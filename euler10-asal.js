e5f4aa1c15f73ac5dbbf57d7b1ae7ba21fea0317
akaymanz <ibrahimakay5183@gmail.com>
2020-06-27 23:12:45 +0300

    değişiklik

---
+++ euler10-asal.js
@@ -0,0 +1,31 @@
+(function euler010() {
+  var maxPrimeValue = 2e+6;
+  var i = maxPrimeValue;
+  var sum = 0;
+
+ 
+  while (i >= 2) {
+    if (isPrime(i) === true) {
+      sum += i;
+    }
+
+    i--;
+  }
+
+  function isPrime(n) {
+    var x = Math.floor(Math.sqrt(n));
+    var j = x;
+
+    while (j >= 2) {
+      if (n % j === 0) {
+        return false;
+      }
+      j--;
+    }
+
+    return true;
+  }
+
+  console.log(sum);
+  return sum;
+}());
